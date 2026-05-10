815f26e391a47d955447e8ca2856cbb29ca592b8
AnctSinps01 <jiyouan_0325@163.com>
2026-05-10 11:06:08 +0800

    test graphics view

--- DTRD-game.pro
+++ DTRD-game.pro
@@ -10,10 +10,12 @@ CONFIG += c++17
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    mygraphicsview.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    mygraphicsview.h
 
 FORMS += \
     mainwindow.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPushButton>
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    // ui 初始化
     ui->setupUi(this);
+
+    ui->battle_field->hide();
+
+
+    // 信号槽
+    connect(ui->start_game, &QPushButton::clicked, this, [&]() {
+        ui->main_menu->hide();
+        ui->battle_field->show();
+    });
 }
 
 MainWindow::~MainWindow()
